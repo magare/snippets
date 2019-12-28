9dc0a161dfef69c88dc0f2859330af74e6b6822c
vilas <vilas@numniolabs.com>
2019-12-28 18:00:28 +0530

    Empty dialog content after add snippet

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ db.version(1).stores({
 let renderAllSnippets = () => {
   db.snippets.toArray(response => {
     let snippetsArray = response.map(i => {
-      let snippet = i.snippet.replace(/(?:\r\n|\r|\n)/g, "<br>");
       return `
       <span style="font-size: 130%;" onclick="handleOnClick('${i.name}')">
       <a href="#" class="badge badge-pill badge-primary">${i.name}</a>
@@ -21,7 +20,6 @@ let renderAllSnippets = () => {
   });
 };
 
-
 // on add snippet button click
 handleOnClick = i => {
   db.snippets.get(i, response => {
@@ -33,9 +31,9 @@ handleOnClick = i => {
 addSnippet = () => {
   let name = document.getElementById("inputSnippetName").value;
   let snippet = document.getElementById("inputSnippet").value;
-  
+
   console.log("name snippet ", name, snippet);
-  
+
   db.snippets
     .add({
       name: name,
@@ -43,11 +41,13 @@ addSnippet = () => {
     })
     .then(response => {
       console.log(response);
-      renderAllSnippets()
+      renderAllSnippets();
+      document.getElementById("inputSnippetName").value = '';
+      document.getElementById("inputSnippet").value = '';
     })
     .catch(error => {
       console.log(error);
     });
-  };
-  
-  renderAllSnippets();
+};
+
+renderAllSnippets();
